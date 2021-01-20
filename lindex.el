0a45e03194e85737f896850a3e71f488c6ae6447
James Boyle <jhhboyle@gmail.com>
2021-01-20 08:44:37 -0500

    Add package and org file

---
+++ lindex.el
@@ -0,0 +1,14 @@
+;;; lindex.el --- A package for managing the import, formatting, and maintenance of links.
+
+;;; Commentary:
+;; 
+
+(require 'eldoc)
+(require 'cl-lib)
+(require 'flymake)
+
+;;; Code:
+
+(provide 'lindex)
+
+;;; lindex.el ends here
